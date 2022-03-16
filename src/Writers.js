5d233d03ee314a1cdde9d7ab4fb37bbe138f4d03
Alhassan-Nazifa <naalhassan174@gmail.com>
2022-03-16 08:17:04 -0700

    first commit

---
+++ src/Writers.js
@@ -0,0 +1,38 @@
+const writers=[
+{
+    id:1,
+    name: "Ahmed Fazil",
+    avatar:"audre_lorde",
+    email:"fazil174@gmail.com",
+    phone:"0546282732",
+},
+{
+    id:2,
+    name: "Ahmed korkor",
+    avatar:"belle_hooks",
+    email:"korkor56@gmail.com",
+    phone:"0546289732",
+},
+{
+    id:3,
+    name: "omar Sadika",
+    avatar:"du_bois",
+    email:"sadika34@gmail.com",
+    phone:"0546252732",
+},
+{
+    id:4,
+    name: "Mohammed Safaraz ",
+    avatar:"james_baldwin",
+    email:"safaraz174@gmail.com",
+    phone:"0546282332",
+},
+{
+    id:5,
+    name: "yusif Salifu",
+    avatar:"maya_angelou",
+    email:"salifu23@gmail.com",
+    phone:"0541282732",
+}
+]
+export default writers;
